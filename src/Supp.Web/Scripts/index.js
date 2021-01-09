960814e04337fb5f095e9c09baaa31e0a7cad527
Szel <szel@eslz.net>
2021-01-09 16:12:32 +0100

    Install and configure webpack, vuejs, sass, milligram

---
+++ src/Supp.Web/Scripts/index.js
@@ -0,0 +1,11 @@
+﻿import 'milligram/dist/milligram.css'
+import Vue from 'vue'
+import App from './main.vue'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue(App).$mount('#app')
+
+
+console.log('test')
---
+++ src/Supp.Web/webpack.config.js
@@ -0,0 +1,47 @@
+﻿const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './Scripts/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'wwwroot/js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: false
+                        }
+                    },
+                    {
+                        // Compiles Sass to CSS
+                        loader: "sass-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin()
+    ]
+}
